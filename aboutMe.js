bccf9b3fa9c763089b1bee5f9726d29910427077
averyramirez <averyyramirez@gmail.com>
2020-11-11 11:58:40 -0800

    Personal Website
    Transferred files from previous repository

---
+++ aboutMe.js
@@ -0,0 +1,18 @@
+// $(function () {
+//     $(window).on('scroll', function () {
+//         if ($(window).scrollTop() > 10) {
+//             $('.navbar').addClass('active');
+//         } else {
+//             $('.navbar').removeClass('active');
+//         }
+//     });
+// });
+
+// $(window).scroll(function () {
+//     if ($(window).scrollTop() >= 50) {
+//         $('.navbar').css('background', 'transparent');
+//     } else {
+//         $('.navbar').css('background', 'black');
+//     }
+// });
+
